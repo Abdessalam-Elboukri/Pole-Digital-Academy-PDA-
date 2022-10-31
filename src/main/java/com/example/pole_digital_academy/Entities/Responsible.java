04ff0577c0399e1121e279de4663fe627a5a258b
abdessalam-elboukri <abdessalamelboukri00@gmail.com>
2022-10-31 14:07:21 -0700

    Add Some Entities
    add user,admin,responsible,participant entities ans also their satus

---
+++ src/main/java/com/example/pole_digital_academy/Entities/Responsible.java
@@ -0,0 +1,30 @@
+package com.example.pole_digital_academy.Entities;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "responsible")
+public class Responsible extends User{
+    private String domaine;
+    @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(name = "res_type", unique = false, referencedColumnName = "id")
+    //@Column(name = "responsible_type")
+    private ResponsibleType res_type;
+
+    public String getDomaine() {
+        return domaine;
+    }
+
+    public void setDomaine(String domaine) {
+        this.domaine = domaine;
+    }
+
+    public ResponsibleType getRes_type() {
+        return res_type;
+    }
+
+    public void setRes_type(ResponsibleType res_type) {
+        this.res_type = res_type;
+    }
+}
