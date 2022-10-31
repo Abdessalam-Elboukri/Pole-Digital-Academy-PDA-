be87ae82d024e0072e2664af5a8fc0585b6f877a
abdessalam-elboukri <abdessalamelboukri00@gmail.com>
2022-10-31 03:36:55 -0700

    add project's structure

---
+++ src/main/java/com/example/pole_digital_academy/Dao/Implimentation/UserDaoImp.java
@@ -0,0 +1,7 @@
+package com.example.pole_digital_academy.Dao.Implimentation;
+
+import com.example.pole_digital_academy.Dao.UserDao;
+
+public class UserDaoImp implements UserDao {
+
+}
---
+++ src/main/java/com/example/pole_digital_academy/Dao/UserDao.java
@@ -0,0 +1,7 @@
+package com.example.pole_digital_academy.Dao;
+
+import com.example.pole_digital_academy.User;
+
+public interface UserDao {
+
+}
