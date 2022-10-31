d52580f3228ae719d1ebec4edbc3145bb384d85e
abdessalam-elboukri <abdessalamelboukri00@gmail.com>
2022-10-31 03:38:57 -0700

    add somme changes in structure

--- src/main/java/com/example/pole_digital_academy/Dao/Implimentation/UserDaoImp.java
+++
@@ -1,7 +0,0 @@
-package com.example.pole_digital_academy.Dao.Implimentation;
-
-import com.example.pole_digital_academy.Dao.UserDao;
-
-public class UserDaoImp implements UserDao {
-
-}
--- src/main/java/com/example/pole_digital_academy/Dao/UserDao.java
+++ src/main/java/com/example/pole_digital_academy/Dao/UserDao.java
@@ -1,6 +1,5 @@
 package com.example.pole_digital_academy.Dao;
 
-import com.example.pole_digital_academy.User;
 
 public interface UserDao {
 
